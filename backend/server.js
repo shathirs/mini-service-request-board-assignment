05f9b0ae8191f07e883e74a516c8891461466b11
shathirs <sheriffshathir@gmail.com>
2026-05-14 19:22:09 +0530

    Add backend folder updates

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const jobRoutes = require('./routes/jobRoutes');
+const errorHandler = require('./middleware/errorMiddleware');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/jobs', jobRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({
+      message: "Route Not Found",
+    });
+  });
+
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
